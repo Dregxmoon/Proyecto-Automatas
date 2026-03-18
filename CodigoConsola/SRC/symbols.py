5a01096b410b40cc1e43bd4fab745ccd0653788f
David Armando Brughera <147428619+David140802@users.noreply.github.com>
2026-03-18 14:18:41 -0700

    Create symbols.py

---
+++ CodigoConsola/SRC/symbols.py
@@ -0,0 +1,12 @@
+# Tabla de símbolos básica del lenguaje
+#Diccionario del programa donde se compararan los datos del script con los tokens predefinidos
+#Sigue la regla "Palabra detectada": "Token relacional"
+TOKENS = {
+    "INICIO": "TK_INICIO",
+    "FIN": "TK_FIN",
+    "AVANZAR": "TK_AVANZAR",
+    "GIRAR": "TK_GIRAR",
+    "DERECHA": "TK_DERECHA",
+    "IZQUIERDA": "TK_IZQUIERDA",
+    "DETENER": "TK_DETENER",
+}
